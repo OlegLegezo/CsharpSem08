1d1a0795b515466fde63548f673630205410d476
OlegLegezo <oleglegezo@yandex.ru>
2022-06-14 20:51:43 +0300

    подсчёт кол-ва повторяющихся цифр в массиве

---
+++ ex57/Program.cs
@@ -0,0 +1,83 @@
+﻿// Составить частотный словарь элементов
+// двумерного массива. Частотный словарь содержит
+// информацию о том, сколько раз встречается элемент
+// входных данных.
+// 25 мин
+// Набор данных Частотный массив
+// { 1, 9, 9, 0, 2, 8, 0, 9 } 0 встречается 2 раза
+// 1 встречается 1 раз
+// 2 встречается 1 раз
+// 8 встречается 1 раз
+// 9 встречается 3 раза
+// 1, 2, 3
+// 4, 6, 1
+// 2, 1, 6
+// 1 встречается 3 раза
+// 2 встречается 2 раз
+// 3 встречается 1 раз
+// 4 встречается 1 раз
+// 6 встречается 2 раза
+
+
+
+int[,] GetArray(int a, int b) //метод заполнения массива целыми числами
+{
+    int[,] array = new int[a, b];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)//0-строки
+    {
+        for (int j = 0; j < array.GetLength(1); j++)//1-столбцы
+        {
+            array[i, j] = rnd.Next(0, 10); //или array[i, j]=rnd.Next(10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array) //метод печати массива
+{
+    for (int i = 0; i < array.GetLength(0); i++)//0-строки
+    {
+        for (int j = 0; j < array.GetLength(1); j++)//1-столбцы
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("введите кол-во строк: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("введите кол-во столбцов: ");
+int n = int.Parse(Console.ReadLine());
+
+int[,] arrayResult1 = GetArray(m, n);
+Console.WriteLine("массив №1:");
+PrintArray(arrayResult1);
+Console.WriteLine();
+
+
+void CountList (int[,] arr)
+{
+    int[] list={0,1,2,3,4,5,6,7,8,9};
+    for (int l = 0; l < list.Length; l++)
+    {
+        int count=0;
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            for (int j = 0; j < arr.GetLength(1); j++)
+            {
+                if (list[l]==arr[i,j])
+                {
+                    count++;
+                }
+            }
+        }
+        if (count!=0)
+        {
+            Console.WriteLine($"{list[l]} встречается в массиве {count} раз");
+        }
+    }
+}
+
+CountList(arrayResult1);
